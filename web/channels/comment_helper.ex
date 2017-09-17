5a213d0abd6ad432d768373fd52d10007b93290d
Serg Buinov <miksself@gmail.com>
2017-09-18 01:26:17 +0700

    finished nine part

---
+++ web/channels/comment_helper.ex
@@ -0,0 +1,46 @@
+defmodule Pxblog.CommentHelper do
+  alias Pxblog.Comment
+  alias Pxblog.Post
+  alias Pxblog.User
+  alias Pxblog.Repo
+  
+  import Ecto, only: [build_assoc, 2]
+
+  def create(%{"postId" => post_id, "body" => body, "author" => author}, _socket) do
+    post = get_post(post_id)
+    changeset = post
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{body: body, author: author})
+
+      Repo.insert(changeset)
+  end
+
+  def approve(%{"postId" => post_id, "commentId" => commentId}, %{assigns: %{user: user_id}}) do
+    authorize_and_perform(post_id, user_id, fn -> 
+      comment = Repo.get!(Comment, comment_id)
+      Repo.delete(comment)
+    end)
+  end
+
+  defp authorize_and_perform(post_id, user_id, action) do
+    post = get_post(post_id)
+    user = get_user(user_id)
+    if is_authorized_user?(user, post) do
+      action.()
+    else
+      {:error, "User is not authorized"}
+    end
+  end
+
+  defp get_user(user_id) do
+    Repo.get!(User, user_id)
+  end
+
+  defp get_post(post_id) do
+    Repo.get!(Post, post_id) |> Repo.preload([:user, :comments])
+  end
+
+  def is_authorized_user?(user, post) do
+    (user && (user.id == post.user_id || Pxblog.RoleChecker.is_admin?(user)))
+  end
+end
